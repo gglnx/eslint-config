800a76e05ae048e53781e6f94ebb821529f6b434
Dennis Morhardt <info@dennismorhardt.de>
2024-04-16 21:27:35 +0200

    🎉 Init project

---
+++ README.md
@@ -0,0 +1,42 @@
+# @gglnx/eslint-config
+
+This is my personal ESLint shared configuration. It's based on the [`eslint-config-airbnb-base`](https://www.npmjs.com/package/eslint-config-airbnb-base) and [`@typescript-eslint/recommended`](https://typescript-eslint.io/users/configs) rules.
+
+## Installation
+
+Install the configuration and all peer dependencies:
+
+```bash
+npx install-peerdeps --dev @gglnx/eslint-config
+```
+
+## Usage
+
+Create an `.eslintrc` file in your project root:
+
+```json
+{
+  "root": true,
+  "extends": [
+    "@gglnx/eslint-config"
+  ]
+}
+```
+
+## Deviations
+
+* `max-len`: Allowing up to 120 chars in a line
+* `lines-between-class-members`: Don't require an empty line between single line class members
+* `no-param-reassign`: Allowing parameters starting with `$` to be reassigned (use this only referenced parameters like DOM elements)
+* `import/extensions`: Don't require file extensions for importing of JavaScript files
+* `import/no-extraneous-dependencies`: Don't report `devDependencies` in config files (`*.config.js`)
+* `no-restricted-syntax`: Allows the usages of `for of` loops
+* `class-methods-use-this`: Remove requirement for using a `this` in class methods
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+Distributed under the [MIT License](https://opensource.org/license/mit). See `LICENSE` for more information.
---
+++ index.cjs
@@ -0,0 +1,80 @@
+module.exports = {
+  extends: 'eslint-config-airbnb-base',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-len': ['error', {
+      code: 120,
+    }],
+    'lines-between-class-members': ['error', 'always', {
+      exceptAfterSingleLine: true,
+    }],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsForRegex: ['^\\$'],
+    }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+      },
+      {
+        selector: 'LabeledStatement',
+      },
+      {
+        selector: 'WithStatement',
+      },
+    ],
+    'class-methods-use-this': ['off'],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      cjs: 'never',
+      ts: 'never',
+    }],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: [
+        '**/__tests__/**',
+        '**/__mocks__/**',
+        '**/*.config.{js,mjs,cjs}',
+        '**/*.config.*.{js,mjs,cjs}',
+      ],
+      optionalDependencies: false,
+    }],
+  },
+  overrides: [
+    {
+      files: '**/*.{js,cjs,mjs}',
+    },
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+    },
+  ],
+  ignorePatterns: [
+    'node_modules',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
